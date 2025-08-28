39e988c534513b854571153c109455d200134378
Atta khan <attam150@gmail.com>
2025-08-28 20:43:36 +0200

    Initial commit

---
+++ InteleApp/InteleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteleAppApp.swift
+//  InteleApp
+//
+//  Created by Atta khan on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct InteleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
